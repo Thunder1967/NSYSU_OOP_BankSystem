63687bfd2227069dd62d4ab126a5165b6205d0f0
Ttttiiiinnnnnnnaaaaaa <a0958901751@gmail.com>
2026-05-10 19:27:11 +0800

    Implement foreign account withdraw

--- src/main/java/nsysu/bank/account/ForeignAccount.java
+++ src/main/java/nsysu/bank/account/ForeignAccount.java
@@ -1,6 +1,8 @@
 package nsysu.bank.account;
 
 import nsysu.util.enumtype.AccountType;
+import nsysu.util.exception.NegativeBalanceException;
+import nsysu.util.sqlaccess.AccountData;
 
 import java.time.Duration;
 import java.util.Date;
@@ -8,7 +10,7 @@ import java.util.Date;
 public class ForeignAccount extends InterestAccount implements CanWithdraw{
 
     public ForeignAccount(String accountId, double rate) {
-        super(accountId, AccountType.SavingsAccount.getStr(), rate);
+        super(accountId, AccountType.USDAccount.getStr(), rate);
         updateBalanceWithInterest();
     }
 
@@ -18,12 +20,26 @@ public class ForeignAccount extends InterestAccount implements CanWithdraw{
 
     @Override
     protected final void updateBalanceWithInterest(){
-        Duration duration = Duration.between(new Date().toInstant(),this.date.toInstant());
-        this.updateBalance(this.balance*this.rate*(duration.toMinutes()));
+        Duration duration = Duration.between(this.date.toInstant(), new Date().toInstant());
+        this.updateBalance(this.balance * this.rate * duration.toMinutes());
     }
 
     @Override
     public boolean withdraw(double amount) {
-        return false;
+        if(!isValidAmount(amount) || !AccountData.transferable(this.getId())){
+            return false;
+        }
+        try{
+            updateBalance(-amount);
+            AccountData.addOneHistory(this.getId(), -amount, "", "withdraw money from foreign account");
+        }
+        catch (NegativeBalanceException e){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidAmount(double amount) {
+        return amount > 0;
     }
 }
